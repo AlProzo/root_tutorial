de8fa4f4d9bd936290c0ac5b9f12b35888b3eeda
AlProzo <124700008+AlProzo@users.noreply.github.com>
2023-02-14 14:18:50 +0100

    Update macro2.C

--- macro2.C
+++ macro2.C
@@ -1,11 +1,12 @@
-void macro2(){
+void macro1(){
    TH2F *h = new TH2F("h","Option COL example ",300,-4,4,300,-20,20);
    h->SetStats(0);
    h->SetContour(200);
    float px, py;
    for (int i = 0; i < 25000000; i++) {
       gRandom->Rannor(px,py);
-      h->Fill(px-1,5*py);
-      h->Fill(2+0.5*px,2*py-10.,0.1);
+      h->Fill(px-1, 5*py);  //Fill(x,y,weight)
+      h->Fill(2+0.5*px, 2*py-10., 0.1);
    }
    h->Draw("colz");
+}
